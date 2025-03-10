c510e61902c246caf89e2ec0c6f0506fb1782c2d
DOMinator.jsx <parthbrajput30@gmail.com>
2025-03-10 19:29:17 +0530

    Update

--- src/app/(auth)/sign-in/[[...rest]]/page.tsx
+++ src/app/(auth)/sign-in/[[...rest]]/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function page() {
   return (
     <div className="flex flex-col flex-1 w-full items-center justify-center">
-      <SignIn />
+      <SignIn forceRedirectUrl={"/logging-account"} />
     </div>
   );
 }
