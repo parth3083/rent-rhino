4da76b9b7933250c618923ddface330fcababe0e
DOMinator.jsx <parthbrajput30@gmail.com>
2025-03-14 01:15:02 +0530

    Update

--- src/app/(dashboard)/(owner-dashboard)/owner-dashboard/(property)/(add-property)/add-property/AddPropertyPageContent.tsx
+++ src/app/(dashboard)/(owner-dashboard)/owner-dashboard/(property)/(add-property)/add-property/AddPropertyPageContent.tsx
@@ -12,43 +12,75 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 enum PROPERTY_STATUS {
   EMPTY = "EMPTY",
   RENTED = "RENTED",
 }
 
+const PROPERTY_DETAILS_VALIDATION = z.object({
+  name: z.string(),
+  address: z.string(),
+  area: z.string(),
+  city: z.string(),
+  state: z.string(),
+  pinCode: z.number(),
+  images: z.array(z.string()),
+  personLimit: z.number(),
+  propertyStatus: z.nativeEnum(PROPERTY_STATUS),
+  rentAmount: z.number(),
+  depositAmount: z.number(),
+});
+
+type propertyDetailsValidation = z.infer<typeof PROPERTY_DETAILS_VALIDATION>;
+
 function AddPropertyPageContent() {
-    const [imagePreviews, setImagePreviews] = useState<string[]>([]);
+  const [imagePreviews, setImagePreviews] = useState<string[]>([]);
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (!e.target.files) return;
-    
-      const files = Array.from(e.target.files);
-      const previews = files.map((file) => URL.createObjectURL(file));
-    
-      // Store image previews and clean up previous ones
-      setImagePreviews((prev: string[]) => {
-        if (prev.length) {
-          prev.forEach((url) => URL.revokeObjectURL(url));
-        }
-        return [...prev, ...previews]; // Append new previews
-      });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+
+    const files = Array.from(e.target.files);
+    const previews = files.map((file) => URL.createObjectURL(file));
+
+    setImagePreviews((prev: string[]) => {
+      if (prev.length) {
+        prev.forEach((url) => URL.revokeObjectURL(url));
+      }
+      return [...prev, ...previews];
+    });
+  };
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    setValue,
+  } = useForm<propertyDetailsValidation>({
+    resolver: zodResolver(PROPERTY_DETAILS_VALIDATION),
+  });
+
+  const onSubmit = (data: propertyDetailsValidation) => {
+    console.log(data);
+  };
+
+  useEffect(() => {
+    return () => {
+      imagePreviews.forEach((url) => URL.revokeObjectURL(url));
     };
-    
-    // Clean up object URLs on unmount
-    useEffect(() => {
-      return () => {
-        imagePreviews.forEach((url) => URL.revokeObjectURL(url));
-      };
-    }, [imagePreviews]);
-
+  }, [imagePreviews]);
 
   return (
     <div className="w-full flex flex-col">
-      <form className="w-full flex flex-col items-start gap-5 lg:gap-8 p-2 lg:p-5">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full flex flex-col items-start gap-5 lg:gap-8 p-2 lg:p-5"
+      >
         {/* Property Information  */}
         <div className="w-full flex items-center  gap-2">
           <h1 className="text-xl font-medium">Property Information</h1>
@@ -57,27 +89,62 @@ function AddPropertyPageContent() {
         <div className="w-full items-center  gap-8 lg:gap-36   flex ">
           <div className="grid w-full max-w-sm items-center gap-1.5">
             <Label htmlFor="name">Property Name</Label>
-            <Input type="text" id="property_name" placeholder="Property Name" />
+            <Input
+              {...register("name")}
+              type="text"
+              id="property_name"
+              placeholder="Property Name"
+            />
           </div>
         </div>
         <div className="w-full items-center  gap-8 lg:gap-36   flex ">
           <div className="grid w-full  items-center gap-1.5">
             <Label htmlFor="name">Property Address</Label>
-            <Textarea placeholder="Type your message here." id="message" />
+            <Textarea
+              {...register("address")}
+              placeholder="Type your message here."
+              id="message"
+            />
           </div>
         </div>
-        <div className="w-full items-center justify-evenly  gap-8 lg:gap-36   flex ">
+        <div className="w-full items-center   gap-8 lg:gap-36   flex ">
           <div className="grid w-full max-w-sm items-center gap-1.5">
             <Label htmlFor="state">State</Label>
-            <Input type="text" placeholder="State name" id="state" />
+            <Input
+              {...register("state")}
+              type="text"
+              placeholder="State name"
+              id="state"
+            />
           </div>
           <div className="grid w-full max-w-sm items-center gap-1.5">
             <Label htmlFor="city">City</Label>
-            <Input type="text" placeholder="City name" id="city" />
+            <Input
+              {...register("city")}
+              type="text"
+              placeholder="City name"
+              id="city"
+            />
+          </div>
+        </div>
+        <div className="w-full items-center   gap-8 lg:gap-36   flex ">
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="state">Area</Label>
+            <Input
+              {...register("area")}
+              type="text"
+              placeholder="Area name"
+              id="area"
+            />
           </div>
           <div className="grid w-full max-w-sm items-center gap-1.5">
             <Label htmlFor="pincode">Pin Code</Label>
-            <Input type="number" placeholder="Pin Code" id="pincode" />
+            <Input
+              {...register("pinCode")}
+              type="number"
+              placeholder="Pin Code"
+              id="pincode"
+            />
           </div>
         </div>
 
@@ -90,6 +157,7 @@ function AddPropertyPageContent() {
           <div className="grid w-full max-w-sm items-center gap-1.5">
             <Label htmlFor="max_occupancy">Maximum Occupancy</Label>
             <Input
+              {...register("personLimit")}
               type="number"
               id="max_occupancy"
               placeholder="enter state name"
@@ -97,7 +165,11 @@ function AddPropertyPageContent() {
           </div>
           <div className="grid w-full max-w-sm items-center gap-1.5">
             <Label htmlFor="property_status">Property Status</Label>
-            <Select>
+            <Select
+              onValueChange={(value: PROPERTY_STATUS) =>
+                setValue("propertyStatus", value)
+              }
+            >
               <SelectTrigger className="w-full max-w-sm">
                 <SelectValue placeholder="Select status" />
               </SelectTrigger>
@@ -119,11 +191,17 @@ function AddPropertyPageContent() {
         <div className="w-full items-center   gap-8 lg:gap-36   flex ">
           <div className="grid w-full max-w-sm items-center gap-1.5">
             <Label htmlFor="rent">Rent amount (₹)</Label>
-            <Input type="number" placeholder="Enter rent amount " id="rent" />
+            <Input
+              {...register("rentAmount")}
+              type="number"
+              placeholder="Enter rent amount "
+              id="rent"
+            />
           </div>
           <div className="grid w-full max-w-sm items-center gap-1.5">
             <Label htmlFor="deposit">Deposit Amount (₹)</Label>
             <Input
+              {...register("depositAmount")}
               type="number"
               placeholder="Enter deposit amount"
               id="deposit"
@@ -139,7 +217,9 @@ function AddPropertyPageContent() {
         <div className="w-full items-center gap-8 lg:gap-36 lg:pr-36  flex ">
           <div className="grid w-full  items-center gap-1.5">
             <Label htmlFor="picture">Property Images</Label>
-            <Input multiple onChange={handleChange} id="picture" type="file" />
+            <Input
+              {...register("images")}
+              multiple onChange={handleChange} id="picture" type="file" />
           </div>
         </div>
         {imagePreviews.length > 0 && (
@@ -160,10 +240,10 @@ function AddPropertyPageContent() {
               ))}
             </CardContent>
           </Card>
-              )}
-              
-              {/* Final Buttons  */}
-              <div className="w-full items-center justify-start mt-5  gap-8 lg:gap-36   flex ">
+        )}
+
+        {/* Final Buttons  */}
+        <div className="w-full items-center justify-start mt-5  gap-8 lg:gap-36   flex ">
           <Button
             type="button"
             className={buttonVariants({
@@ -174,7 +254,6 @@ function AddPropertyPageContent() {
             Cancel
           </Button>
           <Button
-   
             type="submit"
             className={buttonVariants({
               className:
