49c9f1e1488e7c35f25fa231ca397438837c60c3
DOMinator.jsx <parthbrajput30@gmail.com>
2025-03-13 23:22:31 +0530

    No property empty state done and dusted

---
+++ src/app/(dashboard)/(owner-dashboard)/owner-dashboard/(property)/(all-property)/all-properties/page.tsx
@@ -0,0 +1,13 @@
+import DashboardPage from "@/components/DashboardPage";
+import React from "react";
+import AllPropertiesPageContent from "./AllPropertiesPageContent";
+
+function Page() {
+  return (
+    <DashboardPage title="All Properties">
+      <AllPropertiesPageContent />
+    </DashboardPage>
+  );
+}
+
+export default Page;
